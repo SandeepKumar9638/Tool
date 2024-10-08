ece206c091f4e14c127060c217cd7447c3873f5b
sandeep.a <sandeep.a@cgsl-1382.cgsl.com>
2024-10-08 10:26:08 +0530

    changes in pom.xml

--- src/main/java/com/example/ManagementTool/Controller/CompanyController.java
+++ src/main/java/com/example/ManagementTool/Controller/CompanyController.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,8 +14,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.ManagementTool.Entity.Company;
 import com.example.ManagementTool.Service.CompanyService;
-
-import io.swagger.v3.oas.annotations.Operation;
 
 
 @RestController
